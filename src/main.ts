c1ec25ba5f5f4ab6d07ca1cfc977d7484e90f2c8
beekley <brettbeekley@gmail.com>
2022-04-24 20:54:06 -0700

    Test route

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 'use strict';
 
-import Hapi from "@hapi/hapi";
+import Hapi, { Request } from "@hapi/hapi";
 import { Server } from "@hapi/hapi";
 
 export let server: Server;
@@ -8,10 +8,15 @@ export let server: Server;
 export const init = async function(): Promise<Server> {
     server = Hapi.server({
         port: process.env.PORT || 4000,
-        host: '0.0.0.0'
+        host: '127.0.0.1',
     });
 
     // Routes will go here
+    server.route({
+        method: "GET",
+        path: "/",
+        handler: index,
+    });
 
     return server;
 };
@@ -20,6 +25,11 @@ export const start = async function (): Promise<void> {
     console.log(`Listening on ${server.settings.host}:${server.settings.port}`);
     return server.start();
 };
+
+function index(request: Request): string {
+    console.log("Processing request", request.info.id);
+    return "Hello! Nice to have met you.";
+}
 
 process.on('unhandledRejection', (err) => {
     console.error("unhandledRejection");
