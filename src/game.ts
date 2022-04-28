a70d0a1f476ca721414b7b40471137ebfbd36b07
beekley <brettbeekley@gmail.com>
2022-04-28 07:47:48 -0700

    Get preview URL for answer

--- src/game.ts
+++ src/game.ts
@@ -4,25 +4,32 @@ import { SpotifyWebApi } from "spotify-web-api-ts";
 import { Playlist, Track } from "spotify-web-api-ts/types/types/SpotifyObjects";
 
 export async function getTracksForUser(
-  spotify: SpotifyWebApi,
-  userId: string
+    spotify: SpotifyWebApi,
+    userId: string
 ): Promise<Track[]> {
-  const { items } = await spotify.playlists.getUserPlaylists(userId);
-  const playlists: Array<Playlist> = await Promise.all(
-    items.map(async function (p): Promise<Playlist> {
-      return await spotify.playlists.getPlaylist(p.id);
-    })
-  );
-  // Only gets the first 100 tracks of a playlist.
-  // TODO: get all of them instead.
-  const tracks: Array<Track> = [];
-  playlists.forEach(function (p) {
-    // tracks.concat(...p.tracks);
-    p.tracks.items.forEach((i) => {
-      if ((i.track as Track).preview_url) {
-        tracks.push(i.track as Track);
-      }
+    const { items } = await spotify.playlists.getUserPlaylists(userId);
+    const playlists: Array<Playlist> = await Promise.all(
+        items.map(async function (p): Promise<Playlist> {
+            return await spotify.playlists.getPlaylist(p.id);
+        })
+    );
+    // Only gets the first 100 tracks of a playlist.
+    // TODO: get all of them instead.
+    const tracks: Array<Track> = [];
+    playlists.forEach(function (p) {
+        p.tracks.items.forEach((i) => {
+            if ((i.track as Track).preview_url) {
+                tracks.push(i.track as Track);
+            }
+        });
     });
-  });
-  return tracks;
+
+    console.log(await spotify.artists.getArtist(tracks[100].artists[0].id))
+
+    return tracks;
+}
+
+export function pickTrack(tracks: Track[]): Track {
+    const track: Track = tracks[Math.floor(Math.random() * tracks.length)];
+    return track;
 }
--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,8 @@
 import Hapi, { Request } from "@hapi/hapi";
 import { Server } from "@hapi/hapi";
 import { SpotifyWebApi } from "spotify-web-api-ts";
-import { getTracksForUser } from "./game";
+import { Playlist, Track } from "spotify-web-api-ts/types/types/SpotifyObjects";
+import { getTracksForUser, pickTrack } from "./game";
 import secrets from "../secrets";
 
 export let server: Server;
@@ -11,42 +12,45 @@ export let spotify: SpotifyWebApi;
 
 // https://www.solarwinter.net/using-typescript-with-hapi/
 export const init = async function (): Promise<Server> {
-  server = Hapi.server({
-    port: process.env.PORT || 4000,
-    host: "127.0.0.1",
-  });
+    server = Hapi.server({
+        port: process.env.PORT || 4000,
+        host: "127.0.0.1",
+    });
 
-  // Routes will go here
-  server.route({
-    method: "GET",
-    path: "/",
-    handler: index,
-  });
+    // Routes will go here
+    server.route({
+        method: "GET",
+        path: "/userId/{userId}",
+        handler: play,
+    });
 
-  // log in first
-  spotify = new SpotifyWebApi(secrets);
-  const { access_token } = await spotify.getTemporaryAppTokens();
-  spotify.setAccessToken(access_token);
+    // log in first
+    spotify = new SpotifyWebApi(secrets);
+    const { access_token } = await spotify.getTemporaryAppTokens();
+    spotify.setAccessToken(access_token);
 
-  console.log(await getTracksForUser(spotify, "129048914"));
-
-  return server;
+    return server;
 };
 
 export const start = async function (): Promise<void> {
-  console.log(`Listening on ${server.settings.host}:${server.settings.port}`);
-  return server.start();
+    console.log(`Listening on ${server.settings.host}:${server.settings.port}`);
+    return server.start();
 };
 
-async function index(request: Request): Promise<string> {
-  console.log("Processing request", request.info.id);
-  return "Hello! Nice to have met you.";
+async function play(request: Request): Promise<string> {
+    const userId: string = request.params.userId;
+    console.log("Processing request", userId);
+
+    const userTracks: Track[] = await getTracksForUser(spotify, "129048914");
+    const answer: Track = pickTrack(userTracks);
+
+    return answer.preview_url || "no preview url";
 }
 
 process.on("unhandledRejection", (err) => {
-  console.error("unhandledRejection");
-  console.error(err);
-  process.exit(1);
+    console.error("unhandledRejection");
+    console.error(err);
+    process.exit(1);
 });
 
 init().then(() => start());
